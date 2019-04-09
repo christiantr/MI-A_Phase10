80c805be61b56139efe5f67eefef7c633528a573
Franziska Hollauf <zissxd@gmail.com>
2019-04-09 10:49:18 +0200

    added new activity for shuffling

---
+++ app/src/main/java/com/mia/phase10/ShufflingActivity.java
@@ -0,0 +1,13 @@
+package com.mia.phase10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShufflingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shuffling);
+    }
+}
