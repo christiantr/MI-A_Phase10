ccd05eb088222c5dd4fd987603673be6808f0593
Franziska Hollauf <zissxd@gmail.com>
2019-04-10 10:33:59 +0200

    Add: sensor function for shaking detection

--- app/src/main/java/com/mia/phase10/ShufflingActivity.java
+++ app/src/main/java/com/mia/phase10/ShufflingActivity.java
@@ -1,18 +1,51 @@
 package com.mia.phase10;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ShufflingActivity extends AppCompatActivity {
+public class ShufflingActivity extends AppCompatActivity implements SensorEventListener{
+    private SensorManager sManager;
+    private Sensor accelerometer;
+    private View v;
+    private long lastUpdate;
+    float x,y,z;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shuffling);
+
+        v=new View(this);
+
+        sManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        sManager.unregisterListener(this);
+        super.onPause();
     }
 
     public void shuffle(View view){
@@ -22,4 +55,30 @@ public class ShufflingActivity extends AppCompatActivity {
         card.startAnimation(animShuffle);
 
     }
+
+
+    public void onSensorChanged(SensorEvent se) {
+
+        x = se.values[0];
+        y = se.values[1];
+        z = se.values[2];
+
+        float accelerationSquareRoot = (x * x + y * y + z * z) / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+
+        long actualTime = se.timestamp;
+        if (accelerationSquareRoot >= 2)
+        {
+            if (actualTime - lastUpdate < 200) {
+                return;
+            }
+            lastUpdate = actualTime;
+            shuffle(v);
+
+        }
+
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
 }
