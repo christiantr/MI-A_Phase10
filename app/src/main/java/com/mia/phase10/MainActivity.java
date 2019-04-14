b0a622a2451228bc3c6801a0edd61320aeac616d
albin <albin.weiss@outlook.com>
2019-04-14 17:27:38 +0200

    ADD: Drag and Drop function

--- app/src/main/java/com/mia/phase10/MainActivity.java
+++ app/src/main/java/com/mia/phase10/MainActivity.java
@@ -1,14 +1,24 @@
 package com.mia.phase10;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnDragListener, View.OnLongClickListener{
 
     private Button b;
+    private ImageView card;
+    private LinearLayout deck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         b=findViewById(R.id.openShuffling);
+        card = findViewById(R.id.ID_discard_pile);
+        deck = findViewById(R.id.ID_deck);
+
+        card.setTag("DISCARD PILE");
+        card.setOnLongClickListener(this);
+        deck.setOnDragListener(this);
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +45,91 @@ public class MainActivity extends AppCompatActivity {
         //CardStack.mixStack();
         Intent intent = new Intent(this, ShufflingActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        // Create a new ClipData.Item from the ImageView object's tag
+        ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+        // Create a new ClipData using the tag as a label, the plain text MIME type, and
+        // the already-created item. This will create a new ClipDescription object within the
+        // ClipData, and set its MIME type entry to "text/plain"
+        String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        ClipData data = new ClipData(v.getTag().toString(), mimeTypes, item);
+        // Instantiates the drag shadow builder.
+        View.DragShadowBuilder dragshadow = new View.DragShadowBuilder(v);
+        // Starts the drag
+        v.startDrag(data      // data to be dragged
+                , dragshadow  // drag shadow builder
+                , v           // local data about the drag and drop operation
+                , 0      // flags (not currently used, set to 0)
+        );
+        return true;
+    }
+
+
+    // This is the method that the system calls when it dispatches a drag event to the listener.
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        // Defines a variable to store the action type for the incoming event
+        int action = event.getAction();
+        // Handles each of the expected events
+        switch (action) {
+
+            case DragEvent.ACTION_DRAG_STARTED:
+                // Determines if this View can accept the dragged data
+                if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                    return true;
+                }
+                return false;
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+                // Ignore the event
+                return true;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DROP:
+                // Gets the item containing the dragged data
+                ClipData.Item item = event.getClipData().getItemAt(0);
+                // Gets the text data from the item.
+                String dragData = item.getText().toString();
+                // Displays a message containing the dragged data.
+                Toast.makeText(this, "Dragged data is " + dragData, Toast.LENGTH_SHORT).show();
+                // Invalidates the view to force a redraw
+                v.invalidate();
+
+                View vw = (View) event.getLocalState();
+                ViewGroup owner = (ViewGroup) vw.getParent();
+                owner.removeView(vw); //remove the dragged view
+                //caste the view into LinearLayout as our drag acceptable layout is LinearLayout
+                LinearLayout container = (LinearLayout) v;
+                container.addView(vw);//Add the dragged view
+                vw.setVisibility(View.VISIBLE);//finally set Visibility to VISIBLE
+                // Returns true. DragEvent.getResult() will return true.
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+                // Invalidates the view to force a redraw
+                v.invalidate();
+                // Does a getResult(), and displays what happened.
+                if (event.getResult())
+                    Toast.makeText(this, "The drop was handled.", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, "The drop didn't work.", Toast.LENGTH_SHORT).show();
+                // returns true; the value is ignored.
+                return true;
+            // An unknown action type was received.
+            default:
+                Log.e("DragDrop Example", "Unknown action type received by OnDragListener.");
+                break;
+        }
+        return false;
     }
 }
