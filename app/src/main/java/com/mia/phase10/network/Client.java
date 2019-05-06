9258334de9f9aae5631624ab5454f54e08f4ddca
Christian <truden.christian@gmail.com>
2019-05-06 23:59:19 +0200

    ADD: Client.java  basic logic implemented

--- app/src/main/java/com/mia/phase10/network/Client.java
+++ app/src/main/java/com/mia/phase10/network/Client.java
@@ -1,4 +1,56 @@
 package com.mia.phase10.network;
 
-public class Client {
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Client implements Runnable {
+    private static final int SERVER_PORT = 4001;
+    private Socket socket;
+    private ObjectOutputStream out;
+
+    @Override
+    public void run() {
+        startClient("127.0.0.1", SERVER_PORT);
+        sendMessage("Hello World");
+
+    }
+
+    private void startClient(String IP, int port) {
+        try {
+            socket = new Socket(IP, port);
+            out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private void sendMessage(String message) {
+
+
+        Object obj = new TextTransportObject(message);
+        try {
+            out.writeObject(obj);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void closeClient() {
+        try {
+            out.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
