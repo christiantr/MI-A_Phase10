e40a3caf90ff0b26027f31741dd8ce1db86a600d
Walther Salcher <walthersalcher@aon.at>
2019-05-14 11:23:58 +0200

    einfügen actionbar

--- app/src/main/java/com/mia/phase10/PhaseActivity.java
+++ app/src/main/java/com/mia/phase10/PhaseActivity.java
@@ -1,7 +1,10 @@
 package com.mia.phase10;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class PhaseActivity  extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class PhaseActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.phasen_show);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.menu_phasen) {
+            Toast.makeText(getApplicationContext(), "Phase gedrückt!", Toast.LENGTH_LONG).show();
+            Intent PhaseActivity = new Intent(this, PhaseActivity.class);
+            startActivity(PhaseActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
