3a5827ff57dcedbefc3ef234aefdcca9dd5851c1
Christian <truden.christian@gmail.com>
2019-05-29 14:45:32 +0200

    ADD: ConnectionDetails.java into Connection.jav

---
+++ app/src/main/java/com/mia/phase10/network/ConnectionDetails.java
@@ -0,0 +1,13 @@
+package com.mia.phase10.network;
+
+public class ConnectionDetails {
+    private final String username;
+
+    public ConnectionDetails(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
